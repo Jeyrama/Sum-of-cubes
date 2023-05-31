d62500c8da8e724a097ed5a5a56f6d15708ef14e
Jeyrama <126204426+Jeyrama@users.noreply.github.com>
2023-05-31 12:58:35 -0700

    Update Sum-of-cubes.js

--- Sum-of-cubes.js
+++ Sum-of-cubes.js
@@ -21,3 +21,18 @@ function sumCubes(n) {
   return sum;
 }
 
+// or
+
+function sumCubes(n) {
+  if (n == 1) {
+    return n**3;
+  } else {
+    return n**3 + sumCubes(n - 1);
+  }
+}
+
+// or
+
+function sumCubes(n) {
+  return (n * (n + 1) / 2) ** 2;
+}
